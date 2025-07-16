cbbcfa03009c1ceca92c9a2070aeac99a9266835
espacement <147365681+espacement@users.noreply.github.com>
2025-07-16 21:56:40 +0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 
 ]
 
